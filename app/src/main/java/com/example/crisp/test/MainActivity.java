ff96bbf860631a2d11cd815423dd3e7c2310de82
Ssparkii <crispijnw@gmail.com>
2017-12-01 12:29:19 +0100

    thon

--- app/src/main/java/com/example/crisp/test/MainActivity.java
+++ app/src/main/java/com/example/crisp/test/MainActivity.java
@@ -1,9 +1,5 @@
 package com.example.crisp.test;
 
-import android.content.ContentResolver;
-import android.content.Context;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -11,13 +7,15 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import com.example.crisp.test.R;
@@ -40,11 +38,16 @@ public class MainActivity extends AppCompatActivity {
 
     tabLayout = (TabLayout) findViewById(R.id.tabs);
     tabLayout.setupWithViewPager(viewPager);
+    getFiles();
   }
 
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-    return inflater.inflate(R.layout.fragment_one, container, false);
+    View view = inflater.inflate(R.layout.fragment_one, container, false);
+    ListView lv = (ListView)findViewById(R.id.songList);
+    ArrayAdapter adapter = new ArrayAdapter<>(this, R.layout.fragment_one, R.id.songList, songFiles);
+    lv.setAdapter(adapter);
+    adapter.notifyDataSetChanged();
+    return view;
   }
 
   private void setupViewPager(ViewPager viewPager) {
@@ -56,32 +59,71 @@ public class MainActivity extends AppCompatActivity {
   }
 
   class ViewPagerAdapter extends FragmentPagerAdapter {
-    private final List<Fragment> mFragmentList = new ArrayList<>();
-    private final List<String> mFragmentTitleList = new ArrayList<>();
+      private final List<Fragment> mFragmentList = new ArrayList<>();
+      private final List<String> mFragmentTitleList = new ArrayList<>();
 
 
-    public ViewPagerAdapter(FragmentManager manager) {
+      public ViewPagerAdapter(FragmentManager manager) {
       super(manager);
     }
 
-    @Override
-    public Fragment getItem(int position) {
-      return mFragmentList.get(position);
-    }
+      @Override
+      public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+      }
 
-    @Override
-    public int getCount() {
-      return mFragmentList.size();
-    }
+      @Override
+      public int getCount() {
+        return mFragmentList.size();
+      }
 
-    public void addFragment(Fragment fragment, String title) {
-      mFragmentList.add(fragment);
-      mFragmentTitleList.add(title);
-    }
+      public void addFragment(Fragment fragment, String title) {
+         mFragmentList.add(fragment);
+         mFragmentTitleList.add(title);
+      }
 
-    @Override
-    public CharSequence getPageTitle(int position) {
-      return mFragmentTitleList.get(position);
-    }
+      @Override
+      public CharSequence getPageTitle(int position) {
+          return mFragmentTitleList.get(position);
+      }
   }
+
+  private ArrayList<File> songFiles = new ArrayList<File>();
+
+      public void getFiles() {
+        File dir = new File("storage/extSdCard/Music/");
+        boolean success = true;
+
+        if (!dir.exists()) {
+          success = dir.mkdir();
+        }
+
+        File[] tempList = dir.listFiles();
+        List<File> files = new ArrayList<File>(Arrays.asList(tempList));
+
+        for (int i = 0; i < files.size(); i++) {
+
+          File file = files.get(i);
+          String fileExt = splitExt(file.getName());
+
+          if (!file.isDirectory() && fileExt.equals("mp3")) {
+            songFiles.add(file);
+          }
+          else if(file.isDirectory()){
+            File newDir = new File(file.toString());
+            File[] tempList2 = newDir.listFiles();
+            List<File> newFiles = new ArrayList<File>(Arrays.asList(tempList2));
+
+            files.addAll(newFiles);
+          }
+
+        }
+        System.out.println(songFiles);
+      }
+
+  public static String splitExt(String args) {
+    String filenameArray[] = args.split("\\.");
+    return (filenameArray[filenameArray.length-1]);
+  }
+
 }
--- app/src/main/java/com/example/crisp/test/OneFragment.java
+++ app/src/main/java/com/example/crisp/test/OneFragment.java
@@ -6,6 +6,12 @@ import android.provider.ContactsContract;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -22,43 +28,49 @@ public class OneFragment extends Fragment {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    getFiles();
   }
 
-  private ArrayList<File> songFiles = new ArrayList<File>();
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-
-  public void getFiles() {
-    File dir = new File("storage/extSdCard/Music/");
-    boolean success = true;
-
-    if (!dir.exists()) {
-      success = dir.mkdir();
-    }
-
-    File[] tempFiles = dir.listFiles();
-
-    List<File> files = new ArrayList<File>(Arrays.asList(tempFiles));
-    System.out.println(files);
-
-    for (int i = 0; i < files.; i++) {
-      File file = files[i];
-      if (!file.isDirectory()) {
-        songFiles.add(file);
-        System.out.println("Success");
-      }
-      else {
-        File newDir = new File(file.toString());
-        files = newDir.listFiles();
-        System.out.println(file);
-      }
-
-    }
-    System.out.println(songFiles);
+    return inflater.inflate(R.layout.fragment_one, container, false);
   }
 }
 
-
+//  private ArrayList<File> songFiles = new ArrayList<File>();
+//
+//
+//  public void getFiles() {
+//    File dir = new File("storage/extSdCard/Music/");
+//    boolean success = true;
+//
+//    if (!dir.exists()) {
+//      success = dir.mkdir();
+//    }
+//
+//    File[] tempList = dir.listFiles();
+//
+//    List<File> files = new ArrayList<File>(Arrays.asList(tempList));
+//    for (int i = 0; i < files.size(); i++) {
+//      File file = files.get(i);
+//      String fileExt = splitExt(file.getName());
+//      if (!file.isDirectory() && fileExt.equals("mp3")) {
+//        songFiles.add(file);
+//      }
+//      else if(file.isDirectory()){
+//        File newDir = new File(file.toString());
+//        File[] tempList2 = newDir.listFiles();
+//        List<File> newFiles = new ArrayList<File>(Arrays.asList(tempList2));
+//        files.addAll(newFiles);
+//      }
+//
+//    }
+//    System.out.println(songFiles);
+//  }
+//
+//  public static String splitExt(String args) {
+//    String filenameArray[] = args.split("\\.");
+//    return (filenameArray[filenameArray.length-1]);
+//  }
 
 
 
